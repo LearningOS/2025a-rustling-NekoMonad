61195c6188209051996af2bf66429da629258d9e
NekoMonad <124904799+NekoMonad@users.noreply.github.com>
2025-09-17 20:39:49 +0800

    finish all

--- exercises/algorithm/algorithm4.rs
+++ exercises/algorithm/algorithm4.rs
@@ -3,7 +3,6 @@
 	This problem requires you to implement a basic interface for a binary tree
 */
 
-//I AM NOT DONE
 use std::cmp::Ordering;
 use std::fmt::Debug;
 
@@ -50,13 +49,29 @@ where
 
     // Insert a value into the BST
     fn insert(&mut self, value: T) {
-        //TODO
+        match self.root {
+            Some(ref mut node) => node.insert(value),
+            None => self.root = Some(Box::new(TreeNode::new(value))),
+        }
     }
 
     // Search for a value in the BST
     fn search(&self, value: T) -> bool {
-        //TODO
-        true
+        fn search_node<T: Ord>(node: &Option<Box<TreeNode<T>>>, value: T) -> bool {
+            match node {
+                None => false,
+                Some(ref n) => {
+                    if value == n.value {
+                        true
+                    } else if value < n.value {
+                        search_node(&n.left, value)
+                    } else {
+                        search_node(&n.right, value)
+                    }
+                }
+            }
+        }
+        search_node(&self.root, value)
     }
 }
 
@@ -66,7 +81,21 @@ where
 {
     // Insert a node into the tree
     fn insert(&mut self, value: T) {
-        //TODO
+        match value.cmp(&self.value) {
+            Ordering::Less => {
+                match self.left {
+                    Some(ref mut left) => left.insert(value),
+                    None => self.left = Some(Box::new(TreeNode::new(value))),
+                }
+            }
+            Ordering::Greater => {
+                match self.right {
+                    Some(ref mut right) => right.insert(value),
+                    None => self.right = Some(Box::new(TreeNode::new(value))),
+                }
+            }
+            Ordering::Equal => {}
+        }
     }
 }
 
